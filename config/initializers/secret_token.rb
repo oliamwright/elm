aefaa03df751dfa5b183e55502dd3f9ba770b1be
Darrik Mazey <darrik@darmasoft.com>
2010-12-09 09:46:45 -0500

    initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tm::Application.config.secret_token = '3b34316baa633995141566fe0032c92b79bf9a28a5da3581627cb3fe9b63f68c5771ebd4e08d3a354644c2c3b99f4f7f778d0b8569a62f482e8e68f8e84d5890'
