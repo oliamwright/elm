0c57af3ca9fb2e47afbb4ca86fde486218f6d227
Darrik Mazey <darrik@darmasoft.com>
2011-05-24 14:43:40 -0400

    fix user perm issue

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -36,7 +36,7 @@ after_fork do |server, worker|
 
 	begin
 		uid, gid = Process.euid, Process.egid
-		user, group = 'capuser', 'wwwadmin'
+		user, group = 'deploy', 'deploy'
 		target_uid = Etc.getpwnam(user).uid
 		target_gid = Etc.getgrnam(group).gid
 		worker.tmp.chown(target_uid, target_gid)
