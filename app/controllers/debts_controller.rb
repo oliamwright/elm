625098d1842254a8ae9b86213c93e821c1616810
Darrik Mazey <darrik@darmasoft.com>
2011-05-25 12:31:18 -0400

    debt form

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -7,4 +7,18 @@ class DebtsController < ApplicationController
 	def show
 		@debt = current_user.debts.find(params[:id])
 	end
+
+	def new
+		@debt = Debt.new
+		@debt.service_date = DateTime.now
+	end
+
+	def create
+		@debt = Debt.new(params[:debt])
+		@debt.user = current_user
+		@debt.save
+		flash[:notice] = "Debt '#{@debt.name}' created"
+		redirect_to @debt
+	end
+
 end
