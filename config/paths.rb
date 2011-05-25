14db1ae11fe0a2dc284d1d94a9bc46f396f7ff50
Darrik Mazey <darrik@darmasoft.com>
2011-05-25 00:27:15 -0400

    fix paths

--- config/paths.rb
+++ config/paths.rb
@@ -1,3 +1,3 @@
-socket_path = '/var/www/tm/shared/sockets/unicorn.sock'
-pid_path = '/var/www/tm/shared/pids/unicorn.pid'
-app_root = '/var/www/tm/current'
+socket_path = '/var/www/io/shared/sockets/unicorn.sock'
+pid_path = '/var/www/io/shared/pids/unicorn.pid'
+app_root = '/var/www/io/current'
