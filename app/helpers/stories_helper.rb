c2578648aff6828eb8678e5422480edad9f61b16
Darrik Mazey <darrik@darmasoft.com>
2012-01-27 13:11:48 -0500

    detailed log events

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,11 @@
 module StoriesHelper
+
+	def story_url(story)
+		if story.sprint.nil?
+			backlog_project_url(story.project)
+		else
+			project_sprint_url(story.project, story.sprint)
+		end
+	end
+
 end
--- app/helpers/sub_items_helper.rb
+++ app/helpers/sub_items_helper.rb
@@ -1,5 +1,13 @@
 module SubItemsHelper
 
+	def sub_item_url(item)
+		if item.story.sprint.nil?
+			backlog_project_url(item.story.project)
+		else
+			project_sprint_url(item.story.project, item.story.sprint)
+		end
+	end
+
 	def possible_statuses(item)
 		SubItem::STATUSES.select { |s| current_user.can?("from_#{item.status.to_s}_to_#{s.to_s}".to_sym, item) }
 	end
