3976565d9dbc028826f1cb3b45fbedaead6739b0
Darrik Mazey <darrik@darmasoft.com>
2012-02-10 02:04:32 -0500

    divide estimated time by number of people assigned

--- app/models/task_ownership.rb
+++ app/models/task_ownership.rb
@@ -5,6 +5,10 @@ class TaskOwnership < ActiveRecord::Base
 	scope :for_sprint, lambda { |s| joins(:sub_item).joins("INNER JOIN stories ON sub_items.story_id = stories.id").where("stories.sprint_id = ?", s) }
 	scope :not_ignored, joins(:sub_item).where("not status = 'ignored'")
 
+	def assigned_time
+		self.sub_item.estimated_time / self.sub_item.task_ownerships.count
+	end
+
 	def display_actual_time
 		"%0.02f" % self.actual_time
 	end
